e3b49c54941ea2322c3689c6aa112a041dc7b56d
Jinho Kim <jinho@usc.edu>
2018-09-09 14:09:42 -0700

    check touch hyperplanes minor update
    comparison between original and +1 hp

--- check_touch_hyperplane.m
+++ check_touch_hyperplane.m
@@ -1,8 +1,8 @@
+clear
+mouseName = 'JK052';
+sessionName = 'S01';
 
-mouseName = 'JK025';
-sessionName = 'S19';
-
-wDir = 'D:\Jinho_works\Data\WhiskerVideo\';
+wDir = 'L:\tracked\';
 dir = [wDir,mouseName,sessionName];
 
 cd(dir)
@@ -10,8 +10,11 @@ hpFn = [mouseName, sessionName, '_touch_hp.mat'];
 load(hpFn)
 
 wsa = Whisker.WhiskerSignalTrialArray_2pad(dir);
+length(servo_distance_pair)
 %%
-for i = 7
+close all
+figure
+for i = 1
     angle = servo_distance_pair{i}(1);
     dist = servo_distance_pair{i}(2);
     tinds = find(cellfun(@(x) x.angle == angle && x.radialDistance == dist, wsa.trials));
@@ -29,12 +32,24 @@ for i = 7
     points_4d = [points; ones(1,size(points,2))];
     points_2d = A*points_4d;
     points_2d = unique(round(points_2d(1:2,:)',2),'rows');
+    
+    th_4d1 = [touch_hp{i}(1,:) + hp_peaks{i}(1)     + 0;    touch_hp{i}(2:3,:);ones(1,size(touch_hp{i},2))];
+    th_2d1 = A*th_4d1;
+    th_2d1 = unique(th_2d1(1:2,:)','rows');
+    th_4d2 = [touch_hp{i}(1,:) + hp_peaks{i}(2)     + 0;    touch_hp{i}(2:3,:);ones(1,size(touch_hp{i},2))];
+    th_2d2 = A*th_4d2;
+    th_2d2 = unique(th_2d2(1:2,:)','rows');
+    subplot(1,2,1),scatter(points_2d(:,1),points_2d(:,2),'k.'), hold on, scatter(th_2d1(:,1), th_2d1(:,2),'r.'), scatter(th_2d2(:,1), th_2d2(:,2),'r.')
+    title('Original HP')
+    
+    
     th_4d1 = [touch_hp{i}(1,:) + hp_peaks{i}(1)     + 1;    touch_hp{i}(2:3,:);ones(1,size(touch_hp{i},2))];
     th_2d1 = A*th_4d1;
     th_2d1 = unique(th_2d1(1:2,:)','rows');
     th_4d2 = [touch_hp{i}(1,:) + hp_peaks{i}(2)     + 1;    touch_hp{i}(2:3,:);ones(1,size(touch_hp{i},2))];
     th_2d2 = A*th_4d2;
     th_2d2 = unique(th_2d2(1:2,:)','rows');
-    scatter(points_2d(:,1),points_2d(:,2),'k.'), hold on, scatter(th_2d1(:,1), th_2d1(:,2),'r.'), scatter(th_2d2(:,1), th_2d2(:,2),'r.')
+    subplot(1,2,2),scatter(points_2d(:,1),points_2d(:,2),'k.'), hold on, scatter(th_2d1(:,1), th_2d1(:,2),'r.'), scatter(th_2d2(:,1), th_2d2(:,2),'r.')
+    title('+1 HP')    
     
 end
